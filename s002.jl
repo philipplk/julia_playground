26de340509ffe41692eaebec3772dc69211f90fb
Philipp Kinon <philipp.kinon@kit.edu>
2023-11-17 18:06:31 +0100

    template for packages

---
+++ s002.jl
@@ -0,0 +1,14 @@
+using Pkg
+Pkg.add("PkgTemplates")
+
+using PkgTemplates
+
+t = Template(;
+    user="philipplk",
+    authors=["Philipp Kinon", "Julian Karl Bauer"],
+    plugins=[
+        License(name="MIT"),
+        Git(),
+        GitHubActions(),
+    ],
+)
