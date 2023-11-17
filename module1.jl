aa393602d0f9954d68957caa9450461bd6c3fec1
Julian Karl Bauer <juliankarlbauer@gmx.de>
2023-11-17 17:42:54 +0100

    Learn about namespaces and local modules

---
+++ module1.jl
@@ -0,0 +1,6 @@
+module Ourfancymodule
+
+a = sin(1)
+println("here a is $a")
+
+end
--- test.jl
+++ test.jl
@@ -1,3 +1,12 @@
+using Pkg
+Pkg.add("Plots") # Equivalent to Python:`pip install Plots`
+using Plots # Eqivalent to Python`import Plots`
+
+# Julia / Python
+# using package / from package import *
+# import package / import package
+# using .NiceStuff: nice, DOG / from .Niccestuff import nice, DOG
+
 println("I'm excited to learn Julia!")
 
 function advanced_print(; arg1, default1="Huhu")
@@ -5,3 +14,8 @@ function advanced_print(; arg1, default1="Huhu")
 end
 
 advanced_print(arg1="Phil")
+
+include("module1.jl")
+import .Ourfancymodule
+b = Ourfancymodule.a
+println("b=$b")
