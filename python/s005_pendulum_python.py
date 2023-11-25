6b82d48de4a3549398654312e7b88fcabe85463e
Philipp Kinon <philipp.kinon@kit.edu>
2023-11-25 11:21:09 +0100

    refactor

--- python/s005_pendulum_python.py
+++ python/s005_pendulum_python.py
@@ -10,38 +10,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %% system parameters
-g = 9.81
-l = 1
-m = 1
-E = np.diag([1, m * l * l])
-J = np.array([[0, 1], [-1, 0]])
+gravity = 9.81
+length = 1
+mass = 1
+E_matrix = np.diag([1, mass * length**2])
+J_matrix = np.array([[0, 1], [-1, 0]])
 
 # %% integration parameters
-h = 0.05  # timestep size
-t_0 = 0.0  # start time
-t_1 = 3.0  # end time
-NT = int((t_1 - t_0) / h)  # number of time steps
-epsilon = 1e-7  # tolerance of Newton's method (norm of residual vector)
-name_int = "MP"  # EE (explicit Euler), IE (implicit Euler), MP (midpoint)
+timestepsize = 0.05  # timestep size
+time_start = 0.0  # start time
+time_end = 3.0  # end time
+number_timestep = int((time_end - time_start) / timestepsize)  # number of time steps
+newton_epsilon = 1e-7  # tolerance of Newton's method (norm of residual vector)
+integrator = "MP"  # EE (explicit Euler), IE (implicit Euler), MP (midpoint)
 
 # %% initial values
-phi_0 = 0.0  # initial angle
-omega_0 = 1.0  # initial angular velocity
-x_0 = np.array([phi_0, omega_0])  # initial state
-x = np.zeros((NT + 2, 2))  # memory array containing all states during motion
-x[0, :] = x_0  # set initial values into memory array
+angle_initial = 0.0  # initial angle
+velocity_initial = 1.0  # initial angular velocity
+state_initial = np.array([angle_initial, velocity_initial])  # initial state
+state = np.zeros((NT + 2, 2))  # memory array containing all states during motion
+state[0, :] = state_initial  # set initial values into memory array
 
 
 # %% functions
-def compute(x_n1, x_n, name_int):
+def compute(state_n1, state_n, integrator):
     """
     Parameters
     ----------
-    x_n1 : 2x1 array of float64
+    state_n1 : 2x1 array of float64
         state at next timestep t_n+1 (i.e. an unknown).
-    x_n : 2x1 array of float64
+    state_n : 2x1 array of float64
         state at last timestep t_n (i.e. known).
-    name_int : str
+    integrator : str
         Name of the desired integration scheme.
 
     Returns
@@ -52,60 +52,60 @@ def compute(x_n1, x_n, name_int):
         tangent matrix of Newtons method.
 
     """
-    if name_int == "EE":
+    if integrator == "EE":
         # explicit Euler, evaluates z at t_n
-        z_n = get_z_vector(x_n)
-        residual = E @ (x_n1 - x_n) - h * J @ z_n
-        tangent = E
+        z_n = get_z_vector(state_n)
+        residual = E_matrix @ (state_n1 - state_n) - timestepsize * J_matrix @ z_n
+        tangent = E_matrix
 
-    elif name_int == "IE":
+    elif integrator == "IE":
         # implicit Euler, evaluates z at t_n1
-        z_n1 = get_z_vector(x_n1)
-        dzn1_dxn1 = get_Jacobian(x_n1)
-        residual = E @ (x_n1 - x_n) - h * J @ z_n1
-        tangent = E - h * J @ dzn1_dxn1
+        z_n1 = get_z_vector(state_n1)
+        dzn1_dxn1 = get_Jacobian(state_n1)
+        residual = E_matrix @ (state_n1 - state_n) - timestepsize * J_matrix @ z_n1
+        tangent = E_matrix - timestepsize * J_matrix @ dzn1_dxn1
 
-    elif name_int == "MP":
+    elif integrator == "MP":
         # Midpoint rule, evaluates z at t_n05
-        x_n05 = 0.5 * (x_n + x_n1)
-        z_n05 = get_z_vector(x_n05)
-        dzn05_dxn1 = 0.5 * get_Jacobian(x_n1)
-        residual = E @ (x_n1 - x_n) - h * J @ z_n05
-        tangent = E - h * J @ dzn05_dxn1
+        state_n05 = 0.5 * (state_n + state_n1)
+        z_n05 = get_z_vector(state_n05)
+        dzn05_dxn1 = 0.5 * get_Jacobian(state_n1)
+        residual = E_matrix @ (state_n1 - state_n) - timestepsize * J_matrix @ z_n05
+        tangent = E_matrix - timestepsize * J_matrix @ dzn05_dxn1
 
     return residual, tangent
 
 
-def newton_update(x_n1, x_n):
+def newton_update(state_n1, state_n):
     """
     Parameters
     ----------
-    x_n1 : 2x1 array of float64
+    state_n1 : 2x1 array of float64
         state at next timestep t_n+1 (i.e. an unknown).
-    x_n : 2x1 array of float64
+    state_n : 2x1 array of float64
         state at last timestep t_n (i.e. known).
 
     Returns
     -------
-    x_n1 : 2x1 array of float64
+    state_n1 : 2x1 array of float64
         iterated state at next timestep.
 
     """
-    resi = 1e5
-    while resi >= epsilon:
-        R, K = compute(x_n1, x_n, name_int)
-        x_delta = -np.linalg.inv(K) @ R
-        x_n1 = x_n1 + x_delta
-        resi = np.linalg.norm(R)
-    return x_n1
+    residual_norm = 1e5
+    while residual_norm >= newton_epsilon:
+        residual, tangent_matrix = compute(state_n1, state_n, integrator)
+        state_delta = -np.linalg.inv(tangent_matrix) @ residual
+        state_n1 = state_n1 + state_delta
+        residual_norm = np.linalg.norm(R)
+    return state_n1
 
 
-def get_z_vector(x):
+def get_z_vector(state):
     """
 
     Parameters
     ----------
-    x : 2x1 array of float64
+    state : 2x1 array of float64
         state vector x=(q,v).
 
     Returns
@@ -114,17 +114,17 @@ def get_z_vector(x):
         Vector z=z(x).
 
     """
-    q = x[0]
-    v = x[1]
-    return np.array([m * g * l * np.sin(q), v])
+    q = state[0]
+    v = state[1]
+    return np.array([mass * gravity * length * np.sin(q), v])
 
 
-def get_Jacobian(x):
+def get_Jacobian(state):
     """
 
     Parameters
     ----------
-    x : 2x1 array of float64
+    state : 2x1 array of float64
         state vector x=(q,v).
 
     Returns
@@ -133,24 +133,24 @@ def get_Jacobian(x):
         Jacobian of z=z(x), i.e. dz/dx.
 
     """
-    q = x[0]
-    v = x[1]
-    return np.diag([m * g * l * np.cos(q), 1])
+    q = state[0]
+    v = state[1]
+    return np.diag([mass * gravity * length * np.cos(q), 1])
 
 
 # %% time-stepping
-t = t_0
-x_n = x_0
-x_n1 = x_0
-k = 0
-while t < t_1:
-    x_n = x_n1
+time = time_start
+state_n = state_initial
+state_n1 = state_initial
+time_index = 0
+while time < time_end:
+    state_n = state_n1
 
-    x_n1 = newton_update(x_n1, x_n)
-    x[k + 1, :] = x_n1
+    state_n1 = newton_update(state_n1, state_n)
+    state[time_index + 1, :] = state_n1
 
-    t = t + h
-    k = k + 1
+    time = time + timestepsize
+    time_index = time_index + 1
 
 # %% Postprocessing: plot state evolution
-plt.plot(x[:, 0])
+plt.plot(state[:, 0])
