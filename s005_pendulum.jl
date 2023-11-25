5f3cfb3c32f536b3f3c65aab828e1a29817f23e2
Julian Karl Bauer <juliankarlbauer@gmx.de>
2023-11-25 12:09:20 +0100

    Start

---
+++ s005_pendulum.jl
@@ -0,0 +1,12 @@
+import LinearAlgebra as la
+
+# system parameters
+gravity = 9.81
+length = 1.0
+mass = 1.0
+
+E_matric = la.Diagonal([1.0, mass * length^2])
+J_matrix = [[0.0, 1.0], [-1.0, 0.0]]
+
+
+
